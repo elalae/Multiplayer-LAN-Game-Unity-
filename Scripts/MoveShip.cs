20df3e6e79445759541036609c9e81e8fde94d76
Liferi Alexandra - Elena <33804236+elalae@users.noreply.github.com>
2023-01-25 02:56:50 +0200

    Add files via upload

---
+++ Scripts/MoveShip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MoveShip : NetworkBehaviour
+{
+	[SerializeField]
+	private float speed;
+
+	void FixedUpdate ()
+    {
+		if(this.isLocalPlayer)
+        {
+			float movement = Input.GetAxis("Horizontal");	
+			GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
+		}
+	}
+}
---
+++ Scripts/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    public static int scoreValue = 0;
+    public TextMeshProUGUI score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score" + scoreValue;
+    }
+}
